3c293bf5bf2d338a46ab0adb3873de4206fb36a3
Omwomo <eugeneoduor79@gmail.com>
2023-07-11 18:32:36 +0300

    Reverted to our initial code we started with

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Remove book from local storage
+function removeBookFromLocalStorage(title, author) {
+  let books = JSON.parse(localStorage.getItem('books')) || [];
+  books = books.filter((book) => book.title !== title || book.author !== author);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
 function addBook() {
   const titleInput = document.getElementById('titleInput');
   const authorInput = document.getElementById('authorInput');
@@ -6,54 +13,49 @@ function addBook() {
 
   // Check if title or author is empty
   if (title === '' || author === '') {
-
     return; // Stop execution
   }
-    const bookElement = document.createElement('div');
+  const bookElement = document.createElement('div');
 
-    const titleParagraph = document.createElement('p');
-    titleParagraph.innerText = `Title: ${title}`;
-    bookElement.appendChild(titleParagraph);
+  const titleParagraph = document.createElement('p');
+  titleParagraph.innerText = `${title}`;
+  bookElement.appendChild(titleParagraph);
 
-    const authorParagraph = document.createElement('p');
-    authorParagraph.innerText = `Author: ${author}`;
-    bookElement.appendChild(authorParagraph);
+  const authorParagraph = document.createElement('p');
+  authorParagraph.innerText = `${author}`;
+  bookElement.appendChild(authorParagraph);
 
-    const removeButton = document.createElement('button');
-    removeButton.innerText = 'Remove';
-    removeButton.addEventListener('click', () => {
+  const line = document.createElement('hr');
+  bookElement.appendChild(line);
+  // line.appendChild(authorParagraph);
+
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'Remove';
+  removeButton.addEventListener('click', () => {
     bookElement.remove();
-
 
     removeBookFromLocalStorage(title, author);
   });
 
-    // Add line break
-    bookElement.appendChild(document.createElement('br')); 
-    bookElement.appendChild(removeButton);
-  
-    document.getElementById('bookList').appendChild(bookElement);
-  
-    // Store author and title in local storage
-    const book = {
-      title,
-      author,
-    };
-    const books = JSON.parse(localStorage.getItem('books')) || [];
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  
-    // Clear input fields
-    titleInput.value = '';
-    authorInput.value = '';
-  }
-  
-  // Remove book from local storage
-  function removeBookFromLocalStorage(title, author) {
-    let books = JSON.parse(localStorage.getItem('books')) || [];
-    books = books.filter((book) => book.title !== title || book.author !== author);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-  
-  // Call the addBook function somewhere in your code if it is intended to be used
-  addBook();
+  // Add line break
+  bookElement.appendChild(document.createElement('br'));
+  bookElement.appendChild(removeButton);
+
+  document.getElementById('bookList').appendChild(bookElement);
+
+  // Store author and title in local storage
+  const book = {
+    title,
+    author,
+  };
+  const books = JSON.parse(localStorage.getItem('books')) || [];
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+
+  // Clear input fields
+  titleInput.value = '';
+  authorInput.value = '';
+}
+
+// Call the addBook function somewhere in your code if it is intended to be used
+addBook();
