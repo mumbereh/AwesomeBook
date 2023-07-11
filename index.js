2861378c2c57c9f52efd421a4d35a331f7ca3cf7
Omwomo <eugeneoduor79@gmail.com>
2023-07-11 17:50:25 +0300

    Create add book function

--- index.js
+++ index.js
@@ -1,67 +1,29 @@
-let books = JSON.parse(localStorage.getItem('books')) || [];
+function addBook() {
+  const titleInput = document.getElementById('titleInput');
+  const authorInput = document.getElementById('authorInput');
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
 
-function addBook(title, author) {
-  const book = { title, author };
-  books.push(book);
+  // Check if title or author is empty
+  if (title === '' || author === '') {
 
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function removeBook(title) {
-  books = books.filter((book) => book.title !== title);
-
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function removeBookFromLocalStorage(title, author) {
-  let books = JSON.parse(localStorage.getItem('books')) || [];
-  books = books.filter((book) => book.title !== title || book.author !== author);
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function displayBooks() {
-  const bookList = document.getElementById('book-lists');
-  bookList.innerHTML = '';
-
-  books.forEach((book) => {
-    const listItem = document.createElement('li');
-    listItem.style.listStyle = 'none';
-
-    const rmvBtn = document.createElement('button');
-    rmvBtn.textContent = 'Remove';
-    rmvBtn.classList.add('remove-button');
-
-    const line = document.createElement('hr');
-
-    listItem.innerHTML = `${book.title} <br> ${book.author}`;
-    bookList.appendChild(listItem);
-    bookList.appendChild(rmvBtn);
-    bookList.appendChild(line);
-  });
-}
-
-document.getElementById('button').addEventListener('click', () => {
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-
-  addBook(titleInput.value, authorInput.value);
-
-  displayBooks();
-
-  titleInput.value = '';
-  authorInput.value = '';
-});
-
-// Event delegation for the "Remove" button click.
-
-document.getElementById('book-lists').addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove-button')) {
-    const { title } = event.target.dataset;
-
-    removeBook(title);
-
-    displayBooks();
+    return; // Stop execution
   }
-});
+    const bookElement = document.createElement('div');
 
-displayBooks();
+    const titleParagraph = document.createElement('p');
+    titleParagraph.innerText = `Title: ${title}`;
+    bookElement.appendChild(titleParagraph);
+
+    const authorParagraph = document.createElement('p');
+    authorParagraph.innerText = `Author: ${author}`;
+    bookElement.appendChild(authorParagraph);
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.addEventListener('click', () => {
+    bookElement.remove();
+
+
+    removeBookFromLocalStorage(title, author);
+  });
